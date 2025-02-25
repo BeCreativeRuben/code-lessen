0d258c64583ee21815f6d6fd77299522fa6e0571
Ruben Thielman <39592284+BeCreativeRuben@users.noreply.github.com>
2025-02-25 13:50:49 +0100

    .

--- w2/klasopdracht/lichtschakelaar/js/main.js
+++ w2/klasopdracht/lichtschakelaar/js/main.js
@@ -4,6 +4,22 @@
 
 // Hou in een variabele met data type Number bij hoeveel keer de lichtknop werd ingeduwd.
 
+// EXTRA - Haal het element op waarin het aantal keer dat de lichtknop werd ingeduwd moet worden weergegeven
+const switchElement = document.getElementById("timesSwitched");
+switchElement.textContent = timesSwitched;
+
+const knop = document.getElementById('switch');
+let aantalSwitches = document.getElementById("timesSwitched").textContent;
+const knopState = document.getElementById('switchState');
+knop.addEventListener('click', function() {
+    knop.classList.toggle('switched');
+    aantalSwitches++;
+    switchElement.textContent = aantalSwitches;
+    knopState.textContent = knop.classList.contains('switched') ? 'Lamp is aan' : 'Lamp is uit';
+});
+return aantalSwitches;
+
+
 /**
  * Voeg een eventListener toe aan de switch die checkt wanneer de switch verandert.
  * - Update daarin een klasse op een element
@@ -11,6 +27,7 @@
  * - Geef het aantal kliks weer in het witte kader bovenaan links
  * - Pas de boolean aan die checkt of de lamp aan of uit staat
  */
+
 
 /**
  * Voeg een eventListener toe aan de switch die checkt wanneer de cursor wordt ingedrukt
